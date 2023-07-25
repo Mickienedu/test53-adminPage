3abc55791c3aeb355ecc7f9cf9e465ad4efca053
Michael Okorie <mickienedu@gmail.com>
2023-07-25 17:59:41 +0100

    test53-adminPage
    admins page

---
+++ main.js
@@ -0,0 +1,23 @@
+function myFunction() {
+
+    var input, filter, table, tr, h5, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      h5 = tr[i].getElementsByTagName("h5")[0];
+      if (h5) {
+        txtValue = h5.textContent || h5.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+}
+
+// the jquery
